cdd38ed752cda43c421681a943f3c05400ead8bb
VV-I-S <kborisova.priv@gmail.com>
2022-12-03 09:15:04 +0300

    create-NOTFOUND-page

--- client-app/src/pages/NotFound/NotFound.tsx
+++ client-app/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,17 @@
 ﻿import React from "react";
+import style from "./NotFound.module.scss";
 
 const NotFound = () => {
     return (
-        <div>
-            Here
+        <div className={style.tide}>
+            <div>
+                <p className={style.error}>404</p>
+                <p className={style.maybe}>Возможно, эту страницу кто-то уволок...</p>
+            </div>
+            <div>
+            <img src="/img/NotFound.png" />
+            </div>
+
         </div>
     );
 }
