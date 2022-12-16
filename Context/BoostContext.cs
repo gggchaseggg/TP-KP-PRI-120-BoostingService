01350282267a71f67f8119cfc4225e47334b86a8
VV-I-S <kborisova.priv@gmail.com>
2022-12-16 11:33:28 +0300

    create-context

---
+++ Context/BoostContext.cs
@@ -0,0 +1,11 @@
+﻿using BoostingService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoostingService.Context
+{
+    public class BoostContext: DbContext 
+    {
+        public BoostContext(DbContextOptions<BoostContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
