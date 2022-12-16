92c2e9ce7caceedfc54d56f7101f73f18b01d2fd
VV-I-S <kborisova.priv@gmail.com>
2022-12-16 11:32:28 +0300

    little-change

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,6 +3,7 @@ import Main from "./pages/Main/Main";
 import NotFound from "./pages/NotFound/NotFound";
 import Layout from "./component/Layout/Layout";
 import Login from "./pages/Login/Login";
+import Service from "./pages/Service/Service";
 
 import PATHS from "./data/paths";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -19,7 +20,8 @@ export default function App() {
                     <Route path={PATHS.MAIN} element={<Layout />}>
                         <Route path="" element={<Main />} />
                         <Route path="*" element={<NotFound />} />
-                        <Route path={ PATHS.LOGIN } element={<Login />} />
+                        <Route path={PATHS.LOGIN} element={<Login />} />
+                        <Route path={PATHS.SERVICES} element={<Service />} />
                     </Route>
                 </Routes>
             </Router>
