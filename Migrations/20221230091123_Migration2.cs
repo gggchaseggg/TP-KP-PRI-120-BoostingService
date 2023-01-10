0476f9d6f054109fb8598f3dbeaa6d59faebfd30
VV-I-S <kborisova.priv@gmail.com>
2023-01-10 17:02:11 +0300

    add-migration

---
+++ Migrations/20221230091123_Migration2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoostingService.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "mmr",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idUser = table.Column<int>(type: "int", nullable: true),
+                    userid = table.Column<int>(type: "int", nullable: true),
+                    idService = table.Column<int>(type: "int", nullable: true),
+                    serviceid = table.Column<int>(type: "int", nullable: true),
+                    dateOfCreate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    cost = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Services_serviceid",
+                        column: x => x.serviceid,
+                        principalTable: "Services",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_userid",
+                        column: x => x.userid,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_serviceid",
+                table: "Orders",
+                column: "serviceid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_userid",
+                table: "Orders",
+                column: "userid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.AddColumn<int>(
+                name: "mmr",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ Migrations/20230106103236_NewMigration.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoostingService.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "cost",
+                table: "Services",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "boosterid",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "countLP",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "endMMR",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "idBooster",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "startMMR",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_boosterid",
+                table: "Orders",
+                column: "boosterid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_boosterid",
+                table: "Orders",
+                column: "boosterid",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_boosterid",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_boosterid",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "cost",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "boosterid",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "countLP",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "endMMR",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "idBooster",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "startMMR",
+                table: "Orders");
+        }
+    }
+}
