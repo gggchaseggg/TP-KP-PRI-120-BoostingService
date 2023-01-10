9379dca2bd757517e7f516cf417bd7fc07660143
VV-I-S <kborisova.priv@gmail.com>
2023-01-10 17:04:26 +0300

    create-user-profile

---
+++ client-app/src/pages/Profile/Profile.tsx
@@ -0,0 +1,46 @@
+﻿import axios from "axios";
+import React from "react";
+import style from "./Profile.module.scss";
+import UserProfile from "./UserProfile/UserProfile";
+import AdminProfile from "./AdminProfile/AdminProfile";
+import BoosterProfile from "./BoosterProfile/BoosterProfile";
+import { useNavigate } from "react-router-dom";
+
+type UserProfileTypes = {
+    id: number;
+    nickname: string;
+    email: string;
+    phone: string;
+    password: null;
+    role: string;
+}
+
+
+const Profile = () => {
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = React.useState<UserProfileTypes>();
+
+    React.useEffect(() => 
+    { axios.get(`api/account/getUserInfo?email=${localStorage.getItem("email")}`).then(({data})=>setUser(data))}, [])
+
+    return (
+        <div className={style.wrapper}>
+            {(user?.role === "admin")
+                ? <AdminProfile/>
+                : !(user?.role === "booster")
+                    ? <UserProfile />
+                    : <BoosterProfile />
+            }
+            <button className={style.exitButton} onClick={
+                () => {
+                    localStorage.clear();
+                    navigate("/");
+                }
+            }>Выйти
+            </button>
+        </div>
+    );
+}
+export default Profile;
---
+++ client-app/src/pages/Profile/UserProfile/UserProfile.tsx
@@ -0,0 +1,48 @@
+﻿import axios from "axios";
+import React from "react";
+import style from "./UserProfile.module.scss";
+
+type UserProfileTypes = {
+    id: number;
+    nickname: string;
+    email: string;
+    phone: string;
+    password: null;
+    role: string;
+}
+
+
+const UserProfile = () => {
+
+    const [user, setUser] = React.useState<UserProfileTypes>();
+
+    React.useEffect(() => { axios.get(`api/account/getUserInfo?email=${localStorage.getItem("email")}`).then(({ data }) => setUser(data)) }, [])
+
+    return (
+        <div>
+            <div className={style.wrapper_row}>
+                <div className={style.wrapper1_1}>
+                    <div>
+                        <img src="/img/Profile/logo.jpg" alt="лого" className={style.logo} />
+                    </div>
+                    <div className={style.infoUser}>
+                        <h3>{user?.nickname}</h3>
+                        <h5>{user?.email}</h5>
+                        <h5>{user?.phone}</h5>
+                    </div>
+                </div>
+                <div className={style.wrapper1_2}>
+                    <div>
+                        <h4>Текущий заказ</h4>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+export default UserProfile;
