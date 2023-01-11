a25340fbac21a1683d11b85f223f911a86adf73d
VV-I-S <kborisova.priv@gmail.com>
2023-01-11 22:01:44 +0300

    create-booster-profile//add-tables-of-orders&history-orders

--- client-app/src/pages/Profile/BoosterProfile/BoosterProfile.tsx
+++ client-app/src/pages/Profile/BoosterProfile/BoosterProfile.tsx
@@ -3,20 +3,31 @@ import React from "react";
 import style from "./BoosterProfile.module.scss";
 
 type UserProfileTypes = {
-    id: number;
     nickname: string;
     email: string;
     phone: string;
-    password: null;
-    role: string;
+    orders: OrderWaitingTypes[];
 }
+
+type OrderWaitingTypes = {
+    id: number;
+    startMMR: number|null;
+    endMMR: number | null;
+    countLP: number | null;
+    cost: number;
+    status: string;
+}
+
 
 
 const BoosterProfile = () => {
 
     const [user, setUser] = React.useState<UserProfileTypes>();
+    const [newOrder, setNewOrder] = React.useState<UserProfileTypes>();
 
-    React.useEffect(() => { axios.get(`api/account/getUserInfo?email=${localStorage.getItem("email")}`).then(({ data }) => setUser(data)) }, [])
+
+    React.useEffect(() => { axios.get(`api/booster/getBoosterInfo?email=${localStorage.getItem("email")}`).then(({ data }) => setUser(data)) }, [])
+    React.useEffect(() => { axios.get(`api/booster/getNewBoosterInfo?email=${localStorage.getItem("email")}`).then(({ data }) => setNewOrder(data)) }, [])
 
     return (
         <div>
@@ -35,12 +46,59 @@ const BoosterProfile = () => {
                     <div>
                         <h4>Текущий заказ</h4>
                     </div>
+
                     <div>
                     </div>
                 </div>
             </div>
             <div>
-
+                {user?.orders.length !== 0 ? <table className={style.table}>
+                    <thead>
+                        <tr>
+                            <td>Начальный ММР</td>
+                            <td>Конечный ММР</td>
+                            <td>Количество игр SingleDraft</td>
+                            <td>Стоимость</td>
+                            <td>Статус</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {user?.orders.map((item, idx) => <tr key={idx + "userKey"} className={style.userItem}>
+                            <td className={style.userItem__login}>{item.startMMR}</td>
+                            <td className={style.userItem__login}>{item.endMMR}</td>
+                            <td className={style.userItem__login}>{item.countLP}</td>
+                            <td className={style.userItem__login}>{item.cost}</td>
+                            <td className={style.userItem__login}>{item.status}</td>
+                        </tr>)}
+                    </tbody>
+                </table> : <h2>Нет заказов</h2>}
+            </div>
+            <div>
+                {newOrder?.orders.length !== 0 ? <table className={style.table}>
+                    <thead>
+                        <tr>
+                            <td>Начальный ММР</td>
+                            <td>Конечный ММР</td>
+                            <td>Количество игр SingleDraft</td>
+                            <td>Стоимость</td>
+                            <td>Статус</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {newOrder?.orders.map((item, idx) => <tr key={idx + "userKey"} className={style.userItem}>
+                            <td className={style.userItem__login}>{item.startMMR}</td>
+                            <td className={style.userItem__login}>{item.endMMR}</td>
+                            <td className={style.userItem__login}>{item.countLP}</td>
+                            <td className={style.userItem__login}>{item.cost}</td>
+                            <td className={style.userItem__login}>{item.status}</td>
+                            <td>
+                                <button onClick={() => { axios.get(`/api/booster/getNewOrder?email=${localStorage.getItem("email")}&orderid=${item.id}`) }}>Присвоить
+                                </button>
+                            </td>
+                        </tr>)}
+                    </tbody>
+                </table> : <h2>Нет новых заявок</h2>}
             </div>
         </div>
     );
