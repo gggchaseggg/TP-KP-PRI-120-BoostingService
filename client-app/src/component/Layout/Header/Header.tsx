58d8e716ad19ff51a3e8c4765b6fa4d3ef3f1a62
VV-I-S <kborisova.priv@gmail.com>
2023-01-11 22:03:08 +0300

    chsnge-header-by-role

--- client-app/src/component/Layout/Header/Header.tsx
+++ client-app/src/component/Layout/Header/Header.tsx
@@ -1,50 +1,62 @@
-﻿import React, { useState } from "react";
+﻿import React, { useEffect, useState } from "react";
 import style from "./Header.module.scss";
 import { Link, NavLink } from "react-router-dom";
 import PATHS from "../../../data/paths";
 import logo from "./logo.png";
 import HeaderLogin from "./HeaderLogin/HeaderLogin";
+import axios from "axios";
 
 export default function Header(){
    
-    const[isModalVisible, setIsModalVisible] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isServiceVisible, setIsServiceVisible] = useState(true);
 
     const toggleModal = () => {
         setIsModalVisible(wasModalVisible => !wasModalVisible)
     }
 
-        return (
-            <div className={style.wrapper}>
-                <nav className={style.navigation}>
-                    <ul className={style.navList}>
-                        <li className={style.navItem}>
-                            <Link to={PATHS.MAIN}>
-                                <img src={logo} className={style.navItem} />
-                            </Link>
-                        </li>
+    useEffect(() => {
+        let email = localStorage.getItem("email")
+        if (email !== null) axios.get(`/api/account/getuserinfo?email=${email}`).then((r) => {
+            if (r.data.role === "booster") setIsServiceVisible(false)
+        })
+    }, [])
+
+    return (
+        <div className={style.wrapper}>
+            <nav className={style.navigation}>
+                <ul className={style.navList}>
+                    <li className={style.navItem}>
+                        <Link to={PATHS.MAIN}>
+                            <img src={logo} className={style.navItem} />
+                        </Link>
+                    </li>
+                    {isServiceVisible &&
                         <li className={style.navItem}>
                             <NavLink to={PATHS.SERVICES} className={style.colorLink}>Услуги</NavLink>
                         </li>
-                        <li className={style.navItem}>
-                            <NavLink to={PATHS.WARRANTY} className={style.colorLink}>Гарантии</NavLink>
-                        </li>
-                        <li className={style.navItem}>
-                            <NavLink to={PATHS.MONITORING} className={style.colorLink}>Мониторинг</NavLink>
-                        </li>
-                        <li className={style.navItem}>
-                            <NavLink to={PATHS.COMMAND} className={style.colorLink}>Наша команда</NavLink>
-                        </li>
-                        <li className={style.navItem}>
-                            <NavLink to={PATHS.WORK} className={style.colorLink}>Работа</NavLink>
-                        </li>
+                    }
+
+                    <li className={style.navItem}>
+                        <NavLink to={PATHS.WARRANTY} className={style.colorLink}>Гарантии</NavLink>
+                    </li>
+                    <li className={style.navItem}>
+                        <NavLink to={PATHS.MONITORING} className={style.colorLink}>Мониторинг</NavLink>
+                    </li>
+                    <li className={style.navItem}>
+                        <NavLink to={PATHS.COMMAND} className={style.colorLink}>Наша команда</NavLink>
+                    </li>
+                    <li className={style.navItem}>
+                        <NavLink to={PATHS.WORK} className={style.colorLink}>Работа</NavLink>
+                    </li>
+                    <div>
                         <div>
-                            <div>
-                                <HeaderLogin/>
-                                {/*<navlink to={paths.login} classname={style.colorlink}>войти</navlink>*/}
-                            </div>
+                            <HeaderLogin />
+                            {/*<navlink to={paths.login} classname={style.colorlink}>войти</navlink>*/}
                         </div>
-                    </ul>
-                </nav>
-            </div>
-        );
-    };
+                    </div>
+                </ul>
+            </nav>
+        </div>
+     );
+};
