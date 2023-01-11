92dae8ca8597ca43b020be8617db572d9a19c030
VV-I-S <kborisova.priv@gmail.com>
2023-01-11 22:02:16 +0300

    back-for-booster-profile

---
+++ Controllers/BoostController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoostingService.Models.ViewModel;
+using BoostingService.Models;
+using BoostingService.Context;
+
+namespace BoostingService.Controllers
+{
+    [ApiController]
+    [Route("api/booster")]
+    public class BoostController: ControllerBase
+    {
+
+        private readonly BoostContext _context;
+        public BoostController(BoostContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("getBoosterInfo")]
+        public ViewUserModel getOrderInfo(string email)
+        {
+
+            var boostid = _context.Users.Where(u => u.email == email).OrderBy(u => u.id).First().id;
+            var userFromDB = _context.Users.Where(u => u.email == email).OrderBy(u => u.id).FirstOrDefault();
+
+            var orders = _context.Orders
+                .Where(u => u.booster == userFromDB)
+                .OrderByDescending(u => u.dateOfCreate)
+                .Take(10)
+                .ToList();
+
+            List<ListOrder> viewListOrders = new List<ListOrder>();
+
+            foreach (Order order in orders) viewListOrders.Add(
+                    new ListOrder {
+                        id = order.id,
+                        startMMR = order.startMMR,
+                        endMMR = order.endMMR,
+                        countLP = order.countLP,
+                        cost = order.cost,
+                        status = order.status
+                    }
+                    );
+
+
+
+            return new ViewUserModel()
+            {
+                nickname = userFromDB.nickname,
+                email = userFromDB.email,
+                phone = userFromDB.phone,
+                orders = viewListOrders
+            }; 
+        }
+
+        [HttpGet("getNewBoosterInfo")]
+        public ViewUserModel getNewOrderInfo(string email)
+        {
+
+            var boostid = _context.Users.Where(u => u.email == email).OrderBy(u => u.id).First().id;
+            var userFromDB = _context.Users.Where(u => u.email == email).OrderBy(u => u.id).FirstOrDefault();
+
+            var orders = _context.Orders
+                .Where(u => u.status == "Ожидает подтверждения")
+                .ToList();
+
+            List<ListOrder> viewListOrders = new List<ListOrder>();
+
+            foreach (Order order in orders) viewListOrders.Add(
+                    new ListOrder
+                    {
+                        id = order.id,
+                        startMMR = order.startMMR,
+                        endMMR = order.endMMR,
+                        countLP = order.countLP,
+                        cost = order.cost,
+                        status = order.status
+                    }
+                    );
+
+
+
+            return new ViewUserModel()
+            {
+                nickname = userFromDB.nickname,
+                email = userFromDB.email,
+                phone = userFromDB.phone,
+                orders = viewListOrders
+            };
+        }
+
+        [HttpGet("getNewOrder")]
+        public ListOrder getNewOrder(string email, int orderid)
+        {
+
+            var boosterFromDB = _context.Users.Where(u => u.email == email).OrderBy(u => u.id).FirstOrDefault();
+
+
+            var order = _context.Orders
+                .Where(u => u.id == orderid)
+                .FirstOrDefault();
+
+            List<ListOrder> viewListOrders = new List<ListOrder>();
+
+            order.booster = boosterFromDB;
+            order.status = "Ожидает оплаты";
+            _context.SaveChanges();
+
+            return new ListOrder()
+            {
+                id = order.id,
+                startMMR = order.startMMR,
+                endMMR = order.endMMR,
+                countLP = order.countLP,
+                cost = order.cost,
+                status = order.status
+            };
+        }
+    }
+}
