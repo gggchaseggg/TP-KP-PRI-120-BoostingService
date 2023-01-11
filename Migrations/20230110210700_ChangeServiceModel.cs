f8c9ef56ac102d2c60321609cadf24a253e526f8
VV-I-S <kborisova.priv@gmail.com>
2023-01-11 21:59:24 +0300

    add-migration

---
+++ Migrations/20230110210700_ChangeServiceModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoostingService.Migrations
+{
+    public partial class ChangeServiceModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Orders",
+                newName: "status");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "discount",
+                table: "Services",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "discount",
+                table: "Services");
+
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "Orders",
+                newName: "Status");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "name",
+                table: "Services",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Migrations/20230111124521_ChangeServiceAndOrders.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoostingService.Migrations
+{
+    public partial class ChangeServiceAndOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
