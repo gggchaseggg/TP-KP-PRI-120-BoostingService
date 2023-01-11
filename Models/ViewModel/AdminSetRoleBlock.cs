d0f59847d129082e8119d00a3b8c9b5ff65585a2
VV-I-S <kborisova.priv@gmail.com>
2023-01-11 21:57:47 +0300

    create-model-for-admin-panel

---
+++ Models/ViewModel/AdminSetRoleBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoostingService.Models.ViewModel
+{
+    public class AdminSetRoleBlock
+    {
+        public string blockEmail { get; set; }
+        public string blockName { get; set; }
+    }
+}
