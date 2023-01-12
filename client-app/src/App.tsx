c62b9ce84cdfbd0d74cfb3c3439ddf8ac5e8f6f4
VV-I-S <kborisova.priv@gmail.com>
2023-01-12 15:45:04 +0300

    new-depencies

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -5,6 +5,10 @@ import Layout from "./component/Layout/Layout";
 import Login from "./pages/Login/Login";
 import Service from "./pages/Service/Service";
 
+import { useAppDispatch } from "./redux/hooks";
+import axios from "axios";
+import { setUser } from "./redux/userSlice";
+
 import PATHS from "./data/paths";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./util/ScrollToTop";
@@ -13,6 +17,15 @@ import './App.css';
 import Profile from "./pages/Profile/Profile";
 
 export default function App() {
+
+    const dispatch = useAppDispatch();
+
+    React.useEffect(() => {
+        var email = localStorage.getItem("email")
+        if (email !== null) axios.get(`/api/account/getnickname/${email}`).then(({ data }) => dispatch(setUser({ nickname: data })))
+    }, [])
+
+
     return (
         <>
             <Router>
